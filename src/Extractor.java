7782ed5cf0ff4b04a167572e2deaf77a50c493d6
Corey Waldon <cwaldon@myocv.com>
2017-02-12 22:36:40 -0600

    A3

---
+++ src/Extractor.java
@@ -0,0 +1,127 @@
+import java.io.File;
+import java.util.*;
+
+/**
+ * Extractor.java. Implements feature extraction for collinear points in
+ * two dimensional data.
+ *
+ * @author  Dean Hendrix (dh@auburn.edu)
+ * @version 2017-02-08
+ *
+ */
+public class Extractor {
+   
+   /** raw data: all (x,y) points from source data. */
+   private Point[] points;
+   
+   /** lines identified from raw data. */
+   private SortedSet<Line> lines;
+  
+   /**
+    * Builds an extractor based on the points in the file named by filename. 
+    */
+   public Extractor(String filename) {
+      Scanner in = new Scanner(filename);
+      int n = in.nextInt();
+      points = new Point[n];
+      for (int i = 0; i < n; i++) {
+         points[i] = new Point(in.nextInt(), in.nextInt());
+      }
+   }
+  
+   /**
+    * Builds an extractor based on the points in the Collection named by pcoll. 
+    *
+    * THIS METHOD IS PROVIDED FOR YOU AND MUST NOT BE CHANGED.
+    */
+   public Extractor(Collection<Point> pcoll) {
+      points = pcoll.toArray(new Point[]{});
+   }
+  
+   /**
+    * Returns a sorted set of all line segments of exactly four collinear
+    * points. Uses a brute-force combinatorial strategy. Returns an empty set
+    * if there are no qualifying line segments.
+    */
+   public SortedSet<Line> getLinesBrute() {
+      lines = new TreeSet<Line>();
+      SortedSet<Line> checkedLines = new TreeSet<>();
+      combinations(new Point[4], lines, 0, points.length - 1, 0);
+      for (Line l : lines) {
+         Line check = new Line();
+         for (Point aL : l) {
+            if (!check.add(aL)) {
+               break;
+            }
+         }
+         if (check.length() == 4) {
+            checkedLines.add(check);
+         }
+      }
+      return checkedLines;
+   }
+
+
+   private void combinations(Point[] temp, SortedSet<Line> to, int start, int n, int index) {
+      if (index == 4) {
+         to.add(new Line(Arrays.asList(temp)));
+         return;
+      }
+      for (int i = start; i <= n; i++) {
+         temp[index++] = points[i];
+         combinations(temp, to, i + 1, n, index);
+         index--;
+      }
+   }
+  
+   /**
+    * Returns a sorted set of all line segments of at least four collinear
+    * points. The line segments are maximal; that is, no sub-segments are
+    * identified separately. A sort-and-scan strategy is used. Returns an empty
+    * set if there are no qualifying line segments.
+    */
+   public SortedSet<Line> getLinesFast() {
+      lines = new TreeSet<Line>();
+      // Turns out this is just a fancier way of doing brute...
+//      for (int j = 0; j < points.length; j++) {
+//         Point q = points[j];
+//         SortedSet<Double> slopes = new TreeSet<>();
+//         HashMap<Double, SortedSet<Point>> slopeMap = new HashMap<>();
+//         for (int i = 0; i < points.length; i++) {
+//            if (i != j) {
+//               Double slope = q.slopeTo(points[i]);
+//               slopes.add(slope);
+//               if (slopeMap.containsKey(slope)) {
+//                  slopeMap.get(slope).add(points[i]);
+//               } else {
+//                  SortedSet<Point> pointGroup = new TreeSet<>();
+//                  pointGroup.add(points[i]);
+//                  slopeMap.put(slope, pointGroup);
+//               }
+//            }
+//         }
+//         for (Double slope : slopes) {
+//            if (slopeMap.get(slope).size() > 3) {
+//               lines.add(new Line(slopeMap.get(slope)));
+//            }
+//         }
+//      }
+      ArrayList<Point> pointsAL = new ArrayList<>();
+      pointsAL.addAll(Arrays.asList(points));
+      for (int i = 0; i < pointsAL.size(); i++) {
+         Line l = new Line();
+         l.add(pointsAL.get(i));
+         for (Point p : pointsAL) {
+            l.add(p);
+         }
+         if (l.length() > 3) {
+            lines.add(l);
+         }
+         for (Point p : l.line) {
+            pointsAL.remove(p);
+         }
+      }
+      return lines;
+   }
+   
+}
---
+++ src/Line.java
@@ -0,0 +1,174 @@
+import java.util.*;
+
+/**
+ * Line.java
+ * Models a line segment as a sorted set of points.
+ *
+ * @author   Dean Hendrix (dh@auburn.edu)
+ * @version  2017-02-08
+ *
+ */
+public class Line implements Comparable<Line>, Iterable<Point> {
+ 
+   SortedSet<Point> line;
+   
+   /** 
+    * Creates a new line containing no points.
+    *
+    * THIS METHOD IS PROVIDED FOR YOU AND MUST NOT BE CHANGED.
+    */
+   public Line() {
+      line = new TreeSet<>();
+   }
+
+
+   
+   /** 
+    * Creates a new line with containing all distinct collinear points in the
+    * Collection c.
+    */
+   public Line(Collection<Point> c) {
+      line = new TreeSet<>();
+      for (Point p : c) {
+         add(p);
+      }
+   }
+ 
+   /** 
+    * Adds the point p to this line if p is collinear with all points already
+    * in the line and p itself is not already in the line. Returns true if this
+    * line is changed as a result, false otherwise.
+    */
+   public boolean add(Point p) {
+      if (length() == 0) {
+         return line.add(p);
+      }
+      ArrayList<Point> points = new ArrayList<>();
+      line.forEach(points::add);
+      double curSlope = p.slopeTo(points.get(0));
+      for (int i = 1; i < length(); i++) {
+         if (p.slopeTo(points.get(i)) != curSlope) {
+            return false;
+         }
+      }
+
+      return line.add(p);
+   }
+   
+   /** 
+    * Returns the first (minimum) point in this line or null if this line
+    * contains no points.
+    */
+   public Point first() {
+      if (line.size() > 0) {
+         return line.first();
+      }
+      return null;
+   }
+   
+   /** 
+    * Returns the last (maximum) point in this line or null if this line
+    * contains no points.
+    */
+   public Point last() {
+      if (line.size() > 0) {
+         return line.last();
+      }
+      return null;
+   }
+   
+   /** 
+    * Returns the number of points in this line.
+    */
+   public int length() {
+      return line.size();
+   }
+
+   /**
+    * Compares this line with the specified line for order. Returns a negative
+    * integer, zero, or a positive integer if this line is less than, equal to,
+    * or greater than the specified line. Lines are ordered first by their
+    * first point then by their last point. All three properties of compareTo
+    * as specified in the Comparable interface are met, and this implementation
+    * is consistent with equals.
+    */
+   @Override
+   public int compareTo(Line that) {
+      int primary = this.first().compareTo(that.first());
+      int secondary = this.last().compareTo(that.last());
+      if (primary == -1) {
+         return -1;
+      }
+      if (primary == 0) {
+         if (secondary == 0) {
+            return 0;
+         }
+         return -1;
+      }
+      return 1;
+   }
+
+   /** 
+    * Provide an iterator over all the points in this line. The order in which
+    * points are returned must be ascending natural order.
+    */
+   @Override
+   public Iterator<Point> iterator() {
+      return new Iterator<Point>() {
+         private int index = 0;
+         private Point[] points;
+
+         @Override
+         public boolean hasNext() {
+            return index < line.size();
+         }
+
+         @Override
+         public Point next() {
+            if (hasNext()) {
+               Point[] points = new Point[4];
+               line.toArray(points);
+               return points[index++];
+            }
+            return null;
+         }
+      };
+   }
+   
+   /** 
+    * Return true if this point's x and y coordinates are the same as that
+    * point's x and y coordinates, and return false otherwise.
+    *
+    * THIS METHOD IS PROVIDED FOR YOU AND MUST NOT BE CHANGED.
+    */
+   @Override 
+   public boolean equals(Object obj) {
+      if (obj == null) {
+         return false;
+      }
+      if (obj == this) {
+         return true;
+      }
+      if (!(obj instanceof Line)) {
+         return false;
+      }
+      Line that = (Line) obj;
+      return (this.first().equals(that.first())) && (this.last().equals(that.last()));
+   }
+ 
+   /** 
+    * Return a string representation of this line.
+    *
+    * THIS METHOD IS PROVIDED FOR YOU AND MUST NOT BE CHANGED.
+    */
+   @Override
+   public String toString() {
+      StringBuilder s = new StringBuilder();
+      for (Point p : line) {
+         s.append(p + " -> ");
+      }
+      s = s.delete(s.length() - 4, s.length());
+      return s.toString();
+   }
+ 
+}
